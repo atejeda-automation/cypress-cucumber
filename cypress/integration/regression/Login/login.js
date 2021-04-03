bf89f4a16fa6a4f345c078157dee8bf115265ead
Alfred Tejeda <atejeda.ccs@gmail.com>
2021-04-02 20:33:25 -0500

    add logout testcase

--- cypress/integration/regression/Login/login.js
+++ cypress/integration/regression/Login/login.js
@@ -1,4 +1,4 @@
-import { When, Then } from 'cypress-cucumber-preprocessor/steps'
+import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps'
 
 When('I enter the {string} and {string}', async (username, password) => {
     await cy
@@ -19,4 +19,16 @@ Then('i can see the alert with {string}', async (text) => {
     await cy
         .get('#flash')
         .should('to.contain', text)
+})
+
+Given('i click on logout button', async () => {
+    await cy
+        .get('.button')
+        .click()
+})
+
+Then('i can see the logout alert with logout text', async () => {
+    await cy
+        .get('#flash')
+        .should('to.contain', 'You logged out of the secure area!')
 })
